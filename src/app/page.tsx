243beb09104d5daea4d049379701d26f2bd17791
oseye2001 <nemasou96@gmail.com>
2025-11-04 16:54:34 -0400

    Refactor Home component layout and styles
    Updated the main layout with a gradient background and removed FloatingParticles component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// src/app/page.tsx
 "use client";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
@@ -6,21 +7,18 @@ import Formations from "@/components/Formations";
 import FloatingMenu from "@/components/FloatingMenu";
 import Skills from "@/components/Skills";
 import Navbar from "@/components/Navbar";
-import FloatingParticles from "@/components/FloatingParticles";
 
 export default function Home() {
   return (
-    <main className="bg-white text-slate-900 relative">
-  <FloatingParticles />
-  <Navbar />
-  <Hero />
-  <Skills />
-  <Formations />
-  <Projects />
-  <Contact />
-
-  {/* Menu flottant */}
-  <FloatingMenu />
+    <main className="bg-gradient-to-br from-slate-900 via-indigo-900 to-slate-800 text-white">
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Formations />
+      <Projects />
+      <Contact />
+      {/* Menu flottant */}
+      <FloatingMenu />
     </main>
   );
 }
